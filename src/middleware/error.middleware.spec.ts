bb16b8187cc275c8045d69983e439643bb828198
aelsaadawey <aelsaadawey@doh.gov.ae>
2023-03-05 16:08:32 +0400

    feat: introduced gapi updated key

--- src/middleware/error.middleware.spec.ts
+++ src/middleware/error.middleware.spec.ts
@@ -5,6 +5,7 @@ import { HTTPErrorString, HTTPResponses } from 'interfaces/enums';
 describe('error.middleware', () => {
   it('Should success and call res.json', () => {
     global.mockRes.status = jest.fn(() => {});
+
     errorMiddleware(
       new HttpException(HTTPResponses.BusinessError, HTTPErrorString.BadRequest, 'I am additional'),
       global.mockReq,
